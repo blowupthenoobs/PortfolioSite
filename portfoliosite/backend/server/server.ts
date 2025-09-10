7020035bd9cd0b079d5ffd5412e871607e600aad
blowupthenoobs <jeremiah122508@gmail.com>
2025-09-09 23:21:32 -0400

    more testing (again)

--- portfoliosite/backend/server/server.ts
+++ portfoliosite/backend/server/server.ts
@@ -77,6 +77,7 @@ app.use(
     target: "https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev:5173",
     changeOrigin:true,
     ws: true,
+    secure: false,
     // pathRewrite: {"^/drive": "/drive" }
   })
 )
