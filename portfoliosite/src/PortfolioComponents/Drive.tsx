78f113ec04f64e49b2dbf7218a0a0e0c9d7ff689
blowupthenoobs <jeremiah122508@gmail.com>
2025-09-20 12:47:46 -0400

    testing p tag

--- portfoliosite/src/PortfolioComponents/Drive.tsx
+++ portfoliosite/src/PortfolioComponents/Drive.tsx
@@ -3,6 +3,7 @@
 export default function Drive() {
     return (
         <div>
+            <p>putting text here</p>
             <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" width={500} height={500}></iframe>
         </div>
     )
