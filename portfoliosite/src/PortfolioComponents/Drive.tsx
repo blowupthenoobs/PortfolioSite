751631176df1895f405c327b4a49c1c0fe5be550
blowupthenoobs <jeremiah122508@gmail.com>
2025-09-30 18:28:07 -0400

    fixed link

--- portfoliosite/src/PortfolioComponents/Drive.tsx
+++ portfoliosite/src/PortfolioComponents/Drive.tsx
@@ -5,7 +5,7 @@ export default function Drive() {
         <div>
             <p>putting text here</p>
             <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" width={500} height={500}></iframe>
-            <a href="fbalkoewguopotghleendoroibvse.blowupthenoobs.dev">Supposed to send you here (temporary link)</a>
+            <a href="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev">Supposed to send you here (temporary link)</a>
         </div>
     )
 }
