bab4699819072d47ceff520c053001b0e5b610e6
blowupthenoobs <jeremiah122508@gmail.com>
2025-10-01 15:31:59 -0400

    Testing New Blog Post

--- portfoliosite/src/PortfolioComponents/BlogPostThumbnail.tsx
+++ portfoliosite/src/PortfolioComponents/BlogPostThumbnail.tsx
@@ -14,8 +14,9 @@ const BlogPostThumbail: React.FC<BlogPostThumbail> = ({Title, BodyText}) => {
     }
 
     return(
-        <div className={"bg-off-black text-blue-500 border-2 rounded-md border-blue-500 p-10 h-[40%] w-[30%]"} onClick={sendToPage}>
-            <h1 className="mr-2">{Title}</h1>
+        <div className={"bg-off-black text-blue-500 border-2 rounded-md border-blue-500 p-10 min-h-[40%] max-h-[100%] w-[30%]"} onClick={sendToPage}>
+            <h1 className="text-lg">{Title}</h1>
+            <hr className="text-blue-500"/>
             <p>{BodyText} ...</p>
         </div>
     )
