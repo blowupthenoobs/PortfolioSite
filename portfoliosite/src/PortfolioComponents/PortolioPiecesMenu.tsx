b1e5664f5d3be6bb38fc9a0f082a3b6137cb8761
blowupthenoobs <jeremiah122508@gmail.com>
2025-10-01 23:09:28 -0400

    changing to embed

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -3,7 +3,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
-            <iframe frameBorder="0" src="https://itch.io/embed/3775902?bg_color=767676&amp;fg_color=000000&amp;link_color=9ac0ff&amp;border_color=000000" width="552" height="167"><a href="https://lantern-games.itch.io/yet-again">Yet Again by Lantern Games, ABananaDev, blowupthenoobs, JustAMuffinDev</a></iframe>
+            <iframe frameBorder="0" src="https://itch.io/embed-upload/14505244?color=5e7db0" allowfullscreen="" width="980" height="640"><a href="https://lantern-games.itch.io/yet-again">Play Yet Again on itch.io</a></iframe>
         </div>
     )
 }
