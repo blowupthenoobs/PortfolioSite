354dea0369d05561ec7d8618b4165f209c6bbb83
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-10 13:20:57 -0500

    Trying new Stuff

--- portfoliosite/src/PortfolioComponents/PortfolioHeader.tsx
+++ portfoliosite/src/PortfolioComponents/PortfolioHeader.tsx
@@ -1,8 +1,11 @@
 
+interface PortfolioHeader {
+    height?: Float64Array;
+}
 
-export default function PorfolioHeader() {
+const PortfolioHeader: React.FC<PortfolioHeader> = ({height = "15vh"}) => {
     return (
-        <div className="bg-bg-grey w-full h-[15vh] border-b-2 border-blue-500">     {/*w-1/2 is w-[50%], w-full is same as w-[100%]*/}
+        <div className="bg-bg-grey w-full h-[var(height)] border-b-2 border-blue-500">     {/*w-1/2 is w-[50%], w-full is same as w-[100%]*/}
             <div className="flex w-fit m-auto h-full items-center"> 
                 <img src="/images/MischeviousSmugSmile.png" className="h-[90%] rounded-full"/>
                 <h1 className="ml-4 text-[calc(5.5vh)]">blowupthenoobs</h1> {/*text originally 50px*/}
@@ -10,3 +13,5 @@ export default function PorfolioHeader() {
         </div>
     )
 }
+
+export default PortfolioHeader
