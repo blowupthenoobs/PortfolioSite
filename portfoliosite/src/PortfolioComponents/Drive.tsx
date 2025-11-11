b7375fd9ac8f16d3a87e8dd5fc64df59220d311d
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-11 12:51:20 -0500

    Attempting Better Dynamic Sizing

--- portfoliosite/src/PortfolioComponents/Drive.tsx
+++ portfoliosite/src/PortfolioComponents/Drive.tsx
@@ -3,10 +3,10 @@ import PorfolioHeader from "./PortfolioHeader"
 export default function Drive() {
     return (
         <div>
-            <div className="h-[10vh]">
+            <div>
                 <PorfolioHeader height="10vh"/>
             </div>
-            <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" width={"100%"} height={"830vh"}></iframe>
+            <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" width={"100%"} height={"calc(100%-10vh)"}></iframe>
         </div>
     )
 }
