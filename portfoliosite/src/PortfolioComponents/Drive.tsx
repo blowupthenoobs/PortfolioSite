863b31c31af80f9e84f26c9e9abbdc8b2462191c
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-11 13:07:07 -0500

    Trying New Methods

--- portfoliosite/src/PortfolioComponents/Drive.tsx
+++ portfoliosite/src/PortfolioComponents/Drive.tsx
@@ -2,11 +2,9 @@ import PorfolioHeader from "./PortfolioHeader"
 
 export default function Drive() {
     return (
-        <div className="h-full">
-            <div>
-                <PorfolioHeader height="10vh"/>
-            </div>
-            <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" width={"100%"} height={"calc(100%-10vh)"}></iframe>
+        <div className="flex flex-col h-screen">
+            <PorfolioHeader height="10vh"/>
+            <iframe src="https://fbalkoewguopotghleendoroibvse.blowupthenoobs.dev" className="w-full flex-grow"></iframe>
         </div>
     )
 }
