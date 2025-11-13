175eae0d72c85051cd44fa685775eee4a3190af1
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-13 15:38:12 -0500

    Removed backend logging

--- portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
+++ portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
@@ -1,7 +1,6 @@
 export default function HomePortfolioPage() {
 
 const backend = import.meta.env.VITE_BackEndLocation;
-console.log(backend)
 const getData = async () => {
     const res = await fetch(`${backend}/select`)
     console.log(res)
