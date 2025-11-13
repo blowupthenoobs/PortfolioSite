9cee3bebefb3b8bc0a19a5cfc15b871b9f99cd10
blowupthenoobs <jeremiah122508@gmail.com>
2025-11-13 15:24:40 -0500

    and again

--- portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
+++ portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
@@ -1,6 +1,6 @@
 export default function HomePortfolioPage() {
 
-const backend = import.meta.env.BackEndLocation;
+const backend = import.meta.env.VITE_BackEndLocation;
 console.log(backend)
 const getData = async () => {
     const res = await fetch(`${backend}/select`)
