aea2e83a7900ce4ca89aaeb21df78c4c887ea827
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-19 08:36:50 -0500

    Trying More Interfacing

--- portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
+++ portfoliosite/src/PortfolioComponents/HomePortfolioPage.tsx
@@ -1,17 +1,28 @@
+import axios from "axios";
 import getBackendURL from "../utils/getBackendURL";
 
 export default function HomePortfolioPage() {
 
 const backend = getBackendURL();
 const getData = async () => {
-    console.log("starting")
     try{
-        console.log("is trying")
         const res = await fetch(`${backend}/select`)
         const data = await res.json();
         console.log(data)
     } catch(err) {
         console.error('Fetch failed: ', err)
+    }
+
+    try{
+        const payload = {
+            username: "mirrorClone",
+            email: "testingagain@gmail.com",
+            password: "another123"
+        }
+        axios.post(`${backend}/insert`, payload)
+    } catch(err)
+    {
+        console.error('Insertion Failed: ', err)
     }
 
 
