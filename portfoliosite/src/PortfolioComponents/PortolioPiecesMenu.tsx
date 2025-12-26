11dd4a9b82105054e4a4c3cbf9b8e4ce5458e5bc
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 15:39:08 -0500

    Removed Broken code

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -3,7 +3,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
-        <div className={"bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%]"} onClick={sendToPage}>
+        <div className={"bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%]"}>
             <h1 className="text-[30px]">Games</h1>
         </div>
         </div>
