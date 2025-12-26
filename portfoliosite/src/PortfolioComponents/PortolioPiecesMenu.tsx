056a684794899e55aa8030e5f4eea40bda2087aa
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 18:54:04 -0500

    idk what went wrong...

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -2,7 +2,7 @@
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="w-[30%] h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
+        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
             <h1 className="bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Games</h1>
         </div>
     )
