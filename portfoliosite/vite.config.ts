d6763a9617b52a7c0a61243f73c229e38244a057
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 15:14:27 -0500

    Removed Website Caching?

--- portfoliosite/vite.config.ts
+++ portfoliosite/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "./src/config/");
@@ -14,35 +13,6 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       visualizer(),
-      VitePWA({
-        registerType: "autoUpdate",
-        workbox: {
-          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-          navigateFallbackDenylist: [
-            /^\/file-service\/download\//,
-            /^\/file-service\/public\/download\/[^\/]+\/[^\/]+/,
-            /^\/folder-service\/download-zip/,
-            /^\/file-service\/stream-video\/[^\/]+/,
-            /^\/file-service\/download\/[^\/]+/,
-          ],
-          runtimeCaching: [
-            {
-              urlPattern: /\/file-service\/thumbnail\/[a-zA-Z0-9_-]+$/,
-              handler: "CacheFirst",
-              options: {
-                cacheName: "dynamic-thumbnails",
-                expiration: {
-                  maxEntries: 1000,
-                  maxAgeSeconds: 60 * 60 * 24 * 7,
-                },
-                cacheableResponse: {
-                  statuses: [0, 200],
-                },
-              },
-            },
-          ],
-        },
-      }),
     ],
     build: {
       outDir: "dist-frontend",
