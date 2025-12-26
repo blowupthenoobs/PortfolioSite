3cd5e69ab1f0c6786060a673e568314902796761
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 17:38:23 -0500

    Finishing the message

--- portfoliosite/src/PortfolioComponents/HackClubMessage.tsx
+++ portfoliosite/src/PortfolioComponents/HackClubMessage.tsx
@@ -3,8 +3,15 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="">
-            <p>This is my secret message for the hack-clubbers</p>
+            <h3>This is my secret message for the hack-clubbers</h3>
             <p>(namely the ones that are going to be judging this)</p>
+            <br/>
+            <p>I've been mostly working on API and such (hence the very long time spent on this project,) so to show you a bit more of what I've actually been doing I'm gonna give away a few of the secrets</p>
+            <ol>
+                <li>hackclub (where you are right now)</li>
+                <li>accounting (will make some of the other secrets accessable)</li>
+                <li>drive (this stuff is not going to be open to everyone after I finish working on it, but most of my time was spent on here trying to make edits to the source code <a>github.com/subnub/myDrive</a>)</li>
+            </ol>
         </div>
     )
 }
