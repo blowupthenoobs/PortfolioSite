84c7a9db2ca0813365a31811eb3676373a8558a8
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:36:50 -0500

    Updating Some Text Stuff

--- portfoliosite/src/PortfolioComponents/HackClubMessage.tsx
+++ portfoliosite/src/PortfolioComponents/HackClubMessage.tsx
@@ -3,17 +3,20 @@ import PortfolioHeader from "./PortfolioHeader";
 export default function HackClubMessage() {
 
     return (
-        <div className="ml-10">
+        <div>
             <PortfolioHeader/>
-            <h3>This is my secret message for the hack-clubbers</h3>
-            <p>(namely the ones that are going to be judging this)</p>
-            <br/>
-            <p>I've been mostly working on API and such (hence the very long time spent on this project,) so to show you a bit more of what I've actually been doing I'm gonna give away a few of the secrets</p>
-            <ul className="list-disc">
-                <li>hackclub (where you are right now)</li>
-                <li>accounting (will make some of the other secrets accessable)</li>
-                <li>drive (this stuff is not going to be open to everyone after I finish working on it, but most of my time was spent on here trying to make edits to the source code <a>github.com/subnub/myDrive</a>, tho my mongo Atlas thing is currently down for an unknown reason, and thus cannot be used. Source code should work just fine tho)</li>
-            </ul>
+            <div className="ml-10">
+                <h3>This is my secret message for the hack-clubbers</h3>
+                <p>(namely the ones that are going to be judging this)</p>
+                <br/>
+                <p>I've been mostly working on API and such (hence the very long time spent on this project,) so to show you a bit more of what I've actually been doing I'm gonna give away a few of the secrets</p>
+                <ul className="list-disc ml-3">
+                    <li>hackclub (where you are right now)</li>
+                    <li>accounting (will make some of the other secrets accessable)</li>
+                    <li>drive (this stuff is not going to be open to everyone after I finish working on it, but most of my time was spent on here trying to make edits to the source code <a>github.com/subnub/myDrive</a>, tho my mongo Atlas thing is currently down for an unknown reason, and thus cannot be used. Source code should work just fine tho)</li>
+                </ul>
+            </div>
+            
         </div>
     )
 }
--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -8,7 +8,7 @@ export default function PortfolioPiecesMenu() {
     }
 
     return (
-        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
+        <div className="w-full h-full pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
                 <p>Congrats on finding the first secret!</p>
                 <p>You can always come back to this page by typing welcome</p>
                 <p>Here's the way <a onClick={BackToHomePage} className="text-green-400">back to the main site</a></p>
