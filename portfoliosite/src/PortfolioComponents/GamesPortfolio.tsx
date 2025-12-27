66663269b1201dc2dc5b55e26bd6040149474698
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 13:37:29 -0500

    Moving Styling Around

--- portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
+++ portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
@@ -1,7 +1,7 @@
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center pb-[20px]">
+        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] pb-[20px]">
             <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
