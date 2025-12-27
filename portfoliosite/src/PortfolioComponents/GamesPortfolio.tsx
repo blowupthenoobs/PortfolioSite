0d534f853a9b1cde8b9926e9aa7554a3f5f5c9ea
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 13:46:53 -0500

    Currently Broken Stuff

--- portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
+++ portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
@@ -1,11 +1,23 @@
 export default function PortfolioPiecesMenu() {
 
+    interface iframeDetails {
+        source: string;
+        link: string;
+    }
+
+    const game: iframeDetails[] = {
+        {
+            source: "https://itch.io/embed-upload/14505244?color=5e7db0",
+            link: "https://lantern-games.itch.io/yet-again"
+        }
+    }
+
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] flex pb-[20px]">
             <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
-            <iframe src="https://itch.io/embed-upload/14505244?color=5e7db0" allowFullScreen={true} width="980" height="640"><a href="https://lantern-games.itch.io/yet-again">Play Yet Again on itch.io</a></iframe>
+            <iframe src={game[0].source} allowFullScreen={false} width="980" height="640"><a href={game[0].link}>Play Yet Again on itch.io</a></iframe>
             <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
