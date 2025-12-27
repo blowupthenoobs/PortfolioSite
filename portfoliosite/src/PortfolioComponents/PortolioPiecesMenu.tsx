9a937f3fe320ea057fb391cf3b6af2304e06eb6a
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 20:34:55 -0500

    Messing with Portfolio Pieces Design

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -5,7 +5,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div>
-            <div className="w-full h-full pl-[1%] pr-[1%] flex pt-[20px] text-blue-500">
+            <div className="w-full pl-[1%] pr-[1%] flex pt-[20px] text-blue-500">
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Games</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Art</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Models</h1>
