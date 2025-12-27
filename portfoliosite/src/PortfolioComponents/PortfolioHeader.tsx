d0282778badb12d68a754d09e210642454583cc4
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 12:01:29 -0500

    Recoloring Header

--- portfoliosite/src/PortfolioComponents/PortfolioHeader.tsx
+++ portfoliosite/src/PortfolioComponents/PortfolioHeader.tsx
@@ -26,7 +26,7 @@ const PortfolioHeader: React.FC<PortfolioHeader> = ({height = "15vh"}) => {
     getData();
 
     return (
-        <div className="bg-bg-grey w-full max-h-[15vh] h-[full] border-b-2 border-blue-500" style={{height}}>     {/*w-1/2 is w-[50%], w-full is same as w-[100%]*/}
+        <div className="bg-bg-dark-grey w-full max-h-[15vh] h-[full] border-b-2 border-blue-500 text-primary" style={{height}}>     {/*w-1/2 is w-[50%], w-full is same as w-[100%]*/}
             <div className="flex w-fit m-auto h-full items-center"> 
                 <img src="/images/MischeviousSmugSmile.png" className="h-[90%] rounded-full"/>
                 <h1 className="ml-4 text-[calc(5.5vh)]">blowupthenoobs</h1> {/*text originally 50px*/}
