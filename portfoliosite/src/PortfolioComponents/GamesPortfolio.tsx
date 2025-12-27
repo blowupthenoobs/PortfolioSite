c628e406aef6c1ecdb077c263d98d30ac781d4c3
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 14:06:42 -0500

    Added a Couple other Games

--- portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
+++ portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
@@ -2,12 +2,29 @@ export default function PortfolioPiecesMenu() {
     interface iframeDetails {
         source: string;
         link: string;
+        text?: string;
     }
 
     const game: iframeDetails[] = [
         {
             source: "https://itch.io/embed-upload/14505244?color=5e7db0",
             link: "https://lantern-games.itch.io/yet-again"
+        },
+        {
+            source: "https://itch.io/embed-upload/15493256?color=000000",
+            link: "https://blowupthenoobs.itch.io/warpshot"
+        },
+        {
+            source: "https://itch.io/embed-upload/14946514?color=333333",
+            link: "https://blowupthenoobs.itch.io/sole-slinger"
+        },
+        {
+            source: "https://itch.io/embed-upload/15927887?color=3f2832",
+            link: "https://blowupthenoobs.itch.io/speedrunner"
+        },
+        {
+            source: "https://itch.io/embed-upload/8630976?color=333333",
+            link: "https://blowupthenoobs.itch.io/farmerjoe"
         }
     ]
 
@@ -42,7 +59,7 @@ export default function PortfolioPiecesMenu() {
             <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black" onClick={switchGameBack}> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
-            <iframe src={game[selected].source} allowFullScreen={false} width="980" height="640"><a href={game[selected].link}>Play Yet Again on itch.io</a></iframe>
+            <iframe src={game[selected].source} allowFullScreen={false} width="980" height="640"><a href={game[selected].link}>Host Source:</a></iframe>
             <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black" onClick={switchGameForward}> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
