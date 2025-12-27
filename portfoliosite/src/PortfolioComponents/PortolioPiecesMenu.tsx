b2b32f6629a47e23c351a418e9519d68ba3aa3dd
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 19:21:23 -0500

    Trying some resizing extremes

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -3,7 +3,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
-            <h1 className="bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Games</h1>
+            <h1 className="bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Games</h1>
         </div>
     )
 }
