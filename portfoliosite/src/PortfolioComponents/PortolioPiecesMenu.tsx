fd1ba5331fc60c4c19afb58e5fdb416ee89a0a76
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 19:23:35 -0500

    Adding the other tabs

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -2,8 +2,10 @@
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
-            <h1 className="bg-black text-blue-500 border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Games</h1>
+        <div className="w-full h-full pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-blue-500">
+            <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Games</h1>
+            <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Art</h1>
+            <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Models</h1>
         </div>
     )
 }
