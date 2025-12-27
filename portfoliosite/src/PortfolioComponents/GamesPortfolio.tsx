67b240ad339c7a99f14d0e1fad5a9528d3637f5b
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:15:48 -0500

    Restyling Portfolio Stuff

--- portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
+++ portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
@@ -2,7 +2,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
-            <iframe src="https://itch.io/embed-upload/14505244?color=5e7db0" width="980" height="640"><a href="https://lantern-games.itch.io/yet-again">Play Yet Again on itch.io</a></iframe>
+            <iframe src="https://itch.io/embed-upload/14505244?color=5e7db0" allowFullScreen={true} width="980" height="640"><a href="https://lantern-games.itch.io/yet-again">Play Yet Again on itch.io</a></iframe>
         </div>
     )
 }
--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -4,8 +4,8 @@ import GamesPortfolio from "./GamesPortfolio"
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="border-primary border-2 h-[80%]">
-            <div className="w-full pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center">
+        <div className="border-primary border-2 border-b-0 h-[80%] ">
+            <div className="w-[calc(100% + 2px)] pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center">
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Games</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Art</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Models</h1>
