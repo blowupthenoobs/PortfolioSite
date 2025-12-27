350e296b60d3c565421bd61e29ae16ffdb05e320
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-26 19:34:41 -0500

    Bashing My head Against a Wall

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -10,6 +10,7 @@ export default function PortfolioPiecesMenu() {
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Art</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-full text-[30px]">Models</h1>
             </div>
+            
             <GamesPortfolio/>
         </div>
     )
