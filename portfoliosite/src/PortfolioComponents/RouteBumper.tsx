0d396aba17825465df93802ee069c44d1c2af005
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 16:14:32 -0500

    Trying edits to route bumper

--- portfoliosite/src/PortfolioComponents/RouteBumper.tsx
+++ portfoliosite/src/PortfolioComponents/RouteBumper.tsx
@@ -1,19 +1,28 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import getBackendURL from "../utils/getBackendURL";
 import axios from "axios";
+import { useEffect } from "react";
 
-export default async function RouteBumper()  {
+export default function RouteBumper()  {
     const backend = getBackendURL();
     const location = useLocation();
     const navigate = useNavigate();
 
-    const payload = {
-        token:  localStorage.getItem("currentLoginToken"),
-        route: location.pathname
-    }
+    useEffect(() => {
+        const checkPerms = async() => {
+            const payload = {
+                token:  localStorage.getItem("currentLoginToken"),
+                route: location.pathname
+            }
 
-    const perms = (await axios.post(`${backend}/check-perms-for-route`, payload)).data
+            const perms = (await axios.post(`${backend}/check-perms-for-route`, payload)).data
 
-    if(perms !== "hasPerms")
-        navigate("/");
+            if(perms !== "hasPerms")
+                navigate("/");
+        }
+        
+        checkPerms();
+    }, [location.pathname, navigate, backend])
+
+    return null
 };
