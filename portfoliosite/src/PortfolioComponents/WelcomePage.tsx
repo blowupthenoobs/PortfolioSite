e44fee6e774462a1e9c461d3e08ba40cce5c7edb
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:18:54 -0500

    Trying some new changes

--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -11,7 +11,7 @@ export default function PortfolioPiecesMenu() {
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
                 <p>Congrats on finding the first secret!</p>
                 <p>You can always come back to this page by typing welcome</p>
-                <p>Here's the way <a onClick={BackToHomePage}>back to the main site</a></p>
+                <p>Here's the way <a onClick={BackToHomePage} className="text-green-400">back to the main site</a></p>
         </div>
     )
 }
