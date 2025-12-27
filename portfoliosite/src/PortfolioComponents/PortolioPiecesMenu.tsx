783d1086c9490036a9d43ca4f992a8ad5e4b788e
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:45:09 -0500

    More Editorials

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -5,7 +5,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full">
-            <div className="w-full -ml-2 -mr-2 -my-2 pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center place-content-around pb-2"> {/*MessageHere*/}
+            <div className="w-full -ml-2 -mr-2 -my-2 pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center place-content-between pb-4"> {/*MessageHere*/}
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[100%] w-[20%] text-[30px]">Games</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[100%] w-[20%] text-[30px]">Art</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[100%] w-[20%] text-[30px]">Models</h1>
