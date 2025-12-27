3d2367811b1758a90a783410e87bef9ababd8655
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:08:45 -0500

    Fixed

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -4,7 +4,7 @@ import GamesPortfolio from "./GamesPortfolio"
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="border-primary border-2 h-full">
+        <div className="border-primary border-2 h-[80%]">
             <div className="w-full pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center">
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Games</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Art</h1>
--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function PortfolioPiecesMenu() {
+    const navigate = useNavigate();
+
+    const BackToHomePage = () => {
+        navigate("/");
+    }
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
                 <p>Congrats on finding the first secret!</p>
                 <p>You can always come back to this page by typing welcome</p>
+                <p>Here's the way <a onClick={BackToHomePage}>back to the main site</a></p>
         </div>
     )
 }
