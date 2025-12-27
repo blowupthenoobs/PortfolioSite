436bef40310b7f3876451151f8a613e60d0b4d8c
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:03:33 -0500

    Fixed Border?

--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -3,6 +3,7 @@ export default function PortfolioPiecesMenu() {
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
                 <p>Congrats on finding the first secret!</p>
+                <p>You can always come back to this page by typing welcome</p>
         </div>
     )
 }
