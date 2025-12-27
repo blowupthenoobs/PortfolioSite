315506fa99f54c7a5b37023345568bed679b2401
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 13:35:12 -0500

    Adding the Width

--- portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
+++ portfoliosite/src/PortfolioComponents/GamesPortfolio.tsx
@@ -2,11 +2,11 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center pb-[20px]">
-            <div className="h-[30%] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
+            <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
             <iframe src="https://itch.io/embed-upload/14505244?color=5e7db0" allowFullScreen={true} width="980" height="640"><a href="https://lantern-games.itch.io/yet-again">Play Yet Again on itch.io</a></iframe>
-            <div className="h-[30%] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
+            <div className="h-[30%] w-[30px] border-primary border-2 bg-lighter-black"> {/* LeftButton */}
                 {/* Need to find some icon to put here */}
             </div>
         </div>
