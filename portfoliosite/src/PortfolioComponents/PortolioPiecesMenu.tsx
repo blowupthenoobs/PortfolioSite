a192f69cb5e1e9470187c8ef6292865abfe556ab
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 11:27:43 -0500

    Trying Recommended Change

--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -5,7 +5,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full">
-            <div className="w-[calc(100% + 4px)] pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center">
+            <div className="-mx-2 w-full pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-bg-grey text-center">
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Games</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Art</h1>
                 <h1 className="bg-black border-2 rounded-md border-blue-500 p-3 min-h-[40%] max-h-[100%] w-[30%] text-[30px]">Models</h1>
