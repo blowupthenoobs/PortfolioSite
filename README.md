eb66d99c3b665e9af8442adfa79fdf73b7269ca7
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 13:07:53 -0500

    Putting it back to around

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ If you wish to run this website on your computer, you may want to note that ther
     2.) MySQL DataBase (https://github.com/blowupthenoobs/Databasing)
     3.) My myDrive Reskin (https://github.com/blowupthenoobs/driveCloneThing)
 
+To run the first part of the website, after downloading it, you will want to cd into the portfoliosite folder, and there's a chance it's already built in the repo from my idiocies, but if not you should run "npm run build" in the terminal. First you will want to set up the .env file, requiring an entry for After that you can hit "npx vite preview" and it should run the website locally on your computer.
+
 
 
 //Old README
--- portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
+++ portfoliosite/src/PortfolioComponents/PortolioPiecesMenu.tsx
@@ -9,7 +9,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full">
-            <div className="w-full -mx-2 -my-2 pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-off-black text-center place-content-between pb-4"> {/* you can write negative margins by putting the - before them*/}
+            <div className="w-full -mx-2 -my-2 pl-[1%] pr-[1%] flex pt-[20px] text-blue-500 bg-off-black text-center place-content-around pb-4"> {/* you can write negative margins by putting the - before them*/}
                 <h1 className="bg-bg-dark-grey border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[80%] w-[10%] text-[20px]">Games</h1>
                 <h1 className="bg-bg-dark-grey border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[80%] w-[10%] text-[20px]">Art</h1>
                 <h1 className="bg-bg-dark-grey border-2 rounded-md border-blue-500 p-3 min-h-[20%] max-h-[80%] w-[10%] text-[20px]">Models</h1>
