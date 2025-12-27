2ded54b252569cea4cbc89e46b3745f2986b0b6d
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 10:44:48 -0500

    Coloring the text

--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -1,7 +1,7 @@
 export default function PortfolioPiecesMenu() {
 
     return (
-        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center">
+        <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
                 <p>Congrats on finding the first secret!</p>
         </div>
     )
