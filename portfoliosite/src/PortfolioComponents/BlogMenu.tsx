9d2441c7f9680313adab8a1069494420dcb31f25
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-28 01:00:48 -0500

    Going Back to the more "correct" version

--- portfoliosite/src/PortfolioComponents/BlogMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogMenu.tsx
@@ -1,26 +1,30 @@
 import BlogPostThumbail from "./BlogPostThumbnail"
 import getBackendURL from "../utils/getBackendURL";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Blog {
+    title: string;
+    content: string;
+}
 
 export default function BlogMenu() {
     const backend = getBackendURL();
-    const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState<Blog[]>([])
 
-    const getData = async () => {
-        try{
-            const response = await axios.get(`${backend}/get-blogs`)
-            
-            setBlogs(response.data);
-        } catch(err)
-        {
-            console.error('Refresh Failed: ', err)
+    useEffect(() => {
+        const getData = async () => {
+            try{
+                const response = await axios.get(`${backend}/get-blogs`)
+                    
+                setBlogs(response.data);
+            } catch(err) {
+                console.error('Grab failed: ', err)
+            }
         }
 
-
-    }
-
-    getData();
+        getData();
+    }, [backend])
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px]">
