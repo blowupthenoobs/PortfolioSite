14ea249ac2034d2a3062e778488b328f2ce1c3d9
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-27 23:10:23 -0500

    Attempting To Apply Blogs

--- portfoliosite/src/PortfolioComponents/BlogMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogMenu.tsx
@@ -1,10 +1,35 @@
 import BlogPostThumbail from "./BlogPostThumbnail"
+import getBackendURL from "../utils/getBackendURL";
+import axios from "axios";
+import { useState } from "react";
 
 export default function BlogMenu() {
+    const backend = getBackendURL();
+    const [blogs, setBlogs] = useState([])
+
+    const getData = async () => {
+        try{
+            const response = await axios.get(`${backend}/get-blogs`)
+            
+            setBlogs(response.data);
+        } catch(err)
+        {
+            console.error('Refresh Failed: ', err)
+        }
+
+
+    }
+
+    getData();
 
     return (
         <div className="w-full h-full  pl-[1%] pr-[1%] pt-[20px]">
-            <BlogPostThumbail Title="Testing" BodyText="Here's the body text for the first blog thumbnail, just to get a feel for how it'll look like later"/>
+            {blogs.map((item) => {
+                return(
+                    <BlogPostThumbail Title={item.title} BodyText={item.content}/>
+                )
+            })
+            }
         </div>
     )
 }
