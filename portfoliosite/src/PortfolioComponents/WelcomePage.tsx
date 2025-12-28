4c7b21a08e0f9c15c63bb5062c22253670ceb43a
blowupthenoobs <jeremiah122508@gmail.com>
2025-12-28 00:43:58 -0500

    Adding Username mention to Welcome Page

--- portfoliosite/src/PortfolioComponents/WelcomePage.tsx
+++ portfoliosite/src/PortfolioComponents/WelcomePage.tsx
@@ -1,9 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import getBackendURL from "../utils/getBackendURL";
 import RouteBumper from "./RouteBumper";
+import { useState } from "react";
+import axios from "axios";
 
 export default function PortfolioPiecesMenu() {
+    const backend = getBackendURL();
     const navigate = useNavigate();
     RouteBumper();
+
+    const [username, setUsername] = useState(["unknown user"]);
+
+    const getData = async () => {
+        try{
+            const payload = {
+                token: localStorage.getItem("currentLoginToken"),
+            }
+            const response = await axios.post(`${backend}/user-service/get-username`, payload)
+            setUsername(response.data);
+        } catch(err)
+        {
+            console.error('Failed to catch username: ', err)
+        }
+
+
+    }
+
+    getData();
 
     const BackToHomePage = () => {
         navigate("/");
@@ -11,7 +34,7 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full h-full pl-[1%] pr-[1%] pt-[20px] grid place-content-center text-primary">
-                <p>Congrats on finding the first secret!</p>
+                <p>Congrats "{username}" on finding the first secret!</p>
                 <p>You can always come back to this page by typing welcome</p>
                 <p>Here's the way <a onClick={BackToHomePage} className="text-green-400 cursor-pointer">back to the main site</a></p>
         </div>
