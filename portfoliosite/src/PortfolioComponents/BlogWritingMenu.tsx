68a859f3ddfd6cf9d1f88a44f464e616777af1fd
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 17:47:21 -0500

    Trying drastic changes

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -4,10 +4,10 @@ export default function PortfolioPiecesMenu() {
     const backend = getBackendURL();
 
     return (
-        <div className="w-full text-black ml-3">
+        <div className="w-full text-black ml-3 mt-2">
             <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
             <hr className="w-[500px] border-primary"/>
-            <textarea className="w-[calc(100%-25px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
+            <textarea className="w-[calc(100%-50px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
         </div>
     )
 }
