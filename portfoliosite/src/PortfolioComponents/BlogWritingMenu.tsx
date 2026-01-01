5bda3c0e8602e6b3fe007167330cc13dc57eb69f
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 15:49:12 -0500

    fixed?

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -5,9 +5,9 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full text-black ml-3 mb-2">
-            <input type="text" placeholder="Title:"/>
+            <input type="text" placeholder="Title:" className="p-3 text-[30px]"/>
             <hr/>
-            <textarea className="w-(calc(100% - 6px)) mt-2" placeholder="Content:"/>
+            <textarea className="w-[calc(100% - 6px)] mt-2" placeholder="Content:"/>
         </div>
     )
 }
