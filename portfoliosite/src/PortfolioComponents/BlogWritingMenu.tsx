6034faecb07888b6c9bc2685d29899155af9f359
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 16:59:39 -0500

    Added Padding to Content

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -7,7 +7,7 @@ export default function PortfolioPiecesMenu() {
         <div className="w-full text-black ml-3">
             <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
             <hr className="w-[500px] border-primary"/>
-            <textarea className="w-[calc(100%-15px)] mt-2 min-h-[300px]" placeholder="Content:"/>
+            <textarea className="w-[calc(100%-15px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
         </div>
     )
 }
