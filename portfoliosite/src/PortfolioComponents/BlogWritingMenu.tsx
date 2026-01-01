3dc4f4cab414b8ea2c21b0b6be4c29654d4b468d
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 18:02:59 -0500

    Moving the margin

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -4,10 +4,10 @@ export default function PortfolioPiecesMenu() {
     const backend = getBackendURL();
 
     return (
-        <div className="w-full text-black ml-3 mt-2">
-            <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
-            <hr className="w-[500px] border-primary"/>
-            <textarea className="w-[calc(100%-30px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
+        <div className="w-full text-black mt-2">
+            <input type="text" placeholder="Title:" className="p-3 ml-3 text-[30px] mb-2"/>
+            <hr className="w-[503px] border-primary"/>
+            <textarea className="w-[calc(100%-30px)] mt-2 min-h-[300px] p-3 ml-3" placeholder="Content:"/>
         </div>
     )
 }
