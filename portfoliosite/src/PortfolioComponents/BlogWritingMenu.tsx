29554492d0eff092136e4e2c0613b8663feb0f6c
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 15:53:19 -0500

    Messing with the divider

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -6,8 +6,8 @@ export default function PortfolioPiecesMenu() {
     return (
         <div className="w-full text-black ml-3">
             <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
-            <hr/>
-            <textarea className="w-[calc(100%-6px)] mt-2" placeholder="Content:"/>
+            <hr className="w-[50px] border-primary"/>
+            <textarea className="w-[calc(100%-9px)] mt-2" placeholder="Content:"/>
         </div>
     )
 }
