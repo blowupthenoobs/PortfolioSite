8b85d64d603aa66f3555ad7cb18eb4a5b2993c3b
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 15:50:47 -0500

    Put margin in correct spot

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -4,8 +4,8 @@ export default function PortfolioPiecesMenu() {
     const backend = getBackendURL();
 
     return (
-        <div className="w-full text-black ml-3 mb-2">
-            <input type="text" placeholder="Title:" className="p-3 text-[30px]"/>
+        <div className="w-full text-black ml-3">
+            <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
             <hr/>
             <textarea className="w-[calc(100%-6px)] mt-2" placeholder="Content:"/>
         </div>
