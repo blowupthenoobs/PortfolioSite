ea40962fb60d2e1e7fa63e777ff292d04595f2a7
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 17:48:06 -0500

    I think this'll be right

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -7,7 +7,7 @@ export default function PortfolioPiecesMenu() {
         <div className="w-full text-black ml-3 mt-2">
             <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
             <hr className="w-[500px] border-primary"/>
-            <textarea className="w-[calc(100%-50px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
+            <textarea className="w-[calc(100%-30px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
         </div>
     )
 }
