a4a7f3584e3927804c56236a7499cadef51169eb
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-01 18:37:40 -0500

    Stuff

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -5,7 +5,14 @@ export default function PortfolioPiecesMenu() {
 
     return (
         <div className="w-full text-black ml-3 mt-2">
-            <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
+            <div className="flex">
+                <input type="text" placeholder="Title:" className="p-3 text-[30px] mb-2"/>
+                <select>
+                    <option value={"draft"}>draft</option>
+                    <option value={"blog"}>blog</option>
+                    <option value={"snippet"}>snippet</option>
+                </select>
+            </div>
             <hr className="w-[500px] border-primary"/>
             <textarea className="w-[calc(100%-30px)] mt-2 min-h-[300px] p-3" placeholder="Content:"/>
         </div>
