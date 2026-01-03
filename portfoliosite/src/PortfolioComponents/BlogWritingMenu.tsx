2b6f60d53d6f2054d6999fc7454b8e1e824395b6
blowupthenoobs <jeremiah122508@gmail.com>
2026-01-03 16:05:35 -0500

    Rounded buttons a bit

--- portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
+++ portfoliosite/src/PortfolioComponents/BlogWritingMenu.tsx
@@ -46,8 +46,8 @@ export default function PortfolioPiecesMenu() {
             }
 
             <div className="flex mt-5">
-                <button className="bg-secondary-color p-2" onClick={togglePreview}>Preview</button>
-                <button className="bg-link-color p-2 ml-3">Submit</button>
+                <button className="bg-secondary-color p-2 rounded-[2px]" onClick={togglePreview}>Preview</button>
+                <button className="bg-link-color p-2 ml-3 rounded-[2px]">Submit</button>
             </div>
         </div>
     )
